5da1fbf31430a9851e78dd094d9706df89ca7a7a
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-09-06 16:07:20 -0700

    New functions testTriangle and testTriangleShape

--- src/testing/TriangleMain.java
+++ src/testing/TriangleMain.java
@@ -11,21 +11,51 @@ public class TriangleMain {
 	* Perhaps run a txt file as an Object and read the contents to get x,y and z integers.
 	* However for now, run this by running the integers through the terminal.
 	*/
+	private int x;
+	private int y;
+	private int z;
+	
+	public TriangleMain(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+	/*
+	 * This first test tests if the current three integers are a triangle.
+	 */
+	public void testTriangle() {
+		if (x+y > z && x+z > y && y+z > x) {
+			System.out.println("The current integers are a triangle");
+			testTriangleShape();
+		}
+		else {
+			System.out.println("The current integers are not a triangle.");
+			System.exit(0);
+		}
+	}
+	
+	/*
+	 * This method is to test what shape is the triangle.
+	 */
+	public void testTriangleShape() {
+		
+	}
+	
 	public static void main(String[] args) {
 		if (args.length < 3) {
 			System.out.println("Not enough arguments");
-			System.exit(0);;
+			System.exit(0);
 		}
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
 		int z = Integer.parseInt(args[2]);
-		System.out.println("The current integers," + x + "," + y + ", and " + z + ".");
-		//Function goes here. Feel free to change, these are all placeholders.
+		TriangleMain t = new TriangleMain(x,y,z);
+		System.out.println("The current integers are " + x + ", " + y + ", and " + z + ".");
+		t.testTriangle(); //This is where all the tests start. 
 		
 
 	}
 
-	public boolean performFunction (int x, int y, int z) {
-		return false;
-	}
 }
