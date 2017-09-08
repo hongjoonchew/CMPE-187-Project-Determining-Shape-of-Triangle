366a16dd374bf592c5867323ad66a21f746359b2
omvishnu <justin.leinbach@sjsu.edu>
2017-09-08 11:30:21 -0700

    Revert "Added a try/catch statement for integer."
    This reverts commit dd547d42b8e6051f2cc26b40c69394b0fd3acfd5.

--- src/testing/TriangleMain.java
+++ src/testing/TriangleMain.java
@@ -7,9 +7,9 @@ package testing;
  */
 
 public class TriangleMain {
-	private static int x;
-	private static int y;
-	private static int z;
+	private int x;
+	private int y;
+	private int z;
 	
 	public TriangleMain(int x, int y, int z) {
 		this.x = x;
@@ -65,19 +65,9 @@ public class TriangleMain {
 			System.out.println("Not enough arguments");
 			System.exit(0);
 		}
-		try
-		{
-			int x = Integer.parseInt(args[0]);
-			int y = Integer.parseInt(args[1]);
-			int z = Integer.parseInt(args[2]);
-			
-		}
-		catch(NumberFormatException e)
-		{
-			System.out.println("You can only enter an integer");
-			System.exit(0);
-		}
-		
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int z = Integer.parseInt(args[2]);
 		TriangleMain t = new TriangleMain(x,y,z);
 		System.out.println("The current integers are " + x + ", " + y + ", and " + z + ".");
 		t.testTriangle(); //This is where all the tests start. 
